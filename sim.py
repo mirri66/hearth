ee7008ffb6438f9aa4090f11299e6c5dcee68bb8
Grace Tang <tsmgrace@gmail.com>
2015-11-13 00:26:51 +0800

    out of cards penalty, account for negative life total

--- sim.py
+++ sim.py
@@ -24,10 +24,13 @@ def draw_card(deck):
 	deck.pop(idx)
 	return rand_card, deck
 
+
 def main():
-	deck_A = make_deck()
-	deck_B = make_deck()
-	players = {'A': {'life':20, 'card_count':30, 'turn':0}, 'B': {'life':20, 'card_count':30, 'turn':0}}
+	deck = dict()
+	deck['A'] = make_deck()
+	deck['B'] = make_deck()
+	players = {'A': {'life':20, 'card_count':30, 'turn':0, 'card_penalty':0}, 
+			   'B': {'life':20, 'card_count':30, 'turn':0, 'card_penalty':0}}
 	turn = 0
 	current_player= 'A' if random.randint(1,2) == 1 else 'B'
 	# make decks
@@ -41,15 +44,25 @@ def main():
 			mana = 10
 		print current_player + '\'s turn: ' + str(players[current_player]['turn']) + ', mana: ' + str(mana)
 
-		# temporary
-		if players[current_player]['card_count'] == 0:
-			break
+		# draw card, update deck
+		if len(deck[current_player]) > 0:
+			card, new_deck = draw_card(deck[current_player])
+			print 'DRAW CARD'
+			print card
+			deck[current_player] = new_deck
+		else:
+			print 'Player ' + current_player + ' out of cards! Lose ' + str(players[current_player]['card_penalty']) + ' life'
+			players[current_player]['life'] += -1 * players[current_player]['card_penalty']
+			players[current_player]['card_penalty'] += 1
+
+		if 'cost' in card and card['cost'] < mana:
+			print 'PLAY CARD'
 
 		# win condition
-		if players['A']['life'] == 0:
+		if players['A']['life'] <= 0:
 			print 'player B wins'
 			break
-		if players['B']['life'] == 0:
+		if players['B']['life'] <= 0:
 			print 'player A wins'
 			break
 		next_player = 'B' if current_player == 'A' else 'A'
